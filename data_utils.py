051c88d929e4e9bff3c473b6f39284572717e5a5
2KangHo <ho7719@gmail.com>
2019-01-23 18:23:06 +0900

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -30,13 +30,15 @@ class DatasetFromFolder(data.Dataset):
 
         self.input_transform = input_transform
         self.target_transform = target_transform
+        self.add_noise = add_noise
+        self.noise_std = noise_std
 
     def __getitem__(self, index):
         input = load_img(self.image_filenames[index])
         target = input.copy()
         if self.input_transform:
             if self.add_noise:
-                input = noisy(input, noise_std)
+                input = noisy(input, self.noise_std)
             input = self.input_transform(input)
         if self.target_transform:
             target = self.target_transform(target)
