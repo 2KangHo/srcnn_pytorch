4a61037bbc54f0d8cf69ccb47f1d36f1a5e8f949
ho7719 <ho7719@gmail.com>
2019-01-23 15:52:46 +0900

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ from os.path import exists, join, basename
 from os import makedirs, remove
 from six.moves import urllib
 import tarfile
-from torchvision.transforms import Compose, CenterCrop, ToTensor, Scale
+from torchvision.transforms import Compose, RandomCrop, ToTensor, Resize
 
 from data_utils import DatasetFromFolder
 
@@ -37,27 +37,28 @@ def calculate_valid_crop_size(crop_size, upscale_factor):
 
 def input_transform(crop_size, upscale_factor):
     return Compose([
-        CenterCrop(crop_size),
-        Scale(crop_size),
+        RandomCrop(crop_size),
+        Resize((crop_size//(upscale_factor*2), crop_size//upscale_factor)),
         ToTensor(),
     ])
 
 
 def target_transform(crop_size):
     return Compose([
-        CenterCrop(crop_size),
+        RandomCrop(crop_size),
         ToTensor(),
     ])
 
 
 def get_training_set(upscale_factor):
     root_dir = download_bsd300()
-    
+
     train_dir = join(root_dir, "train")
     crop_size = calculate_valid_crop_size(256, upscale_factor)
 
     return DatasetFromFolder(train_dir,
-                             input_transform=input_transform(crop_size, upscale_factor),
+                             input_transform=input_transform(
+                                 crop_size, upscale_factor),
                              target_transform=target_transform(crop_size))
 
 
@@ -67,5 +68,6 @@ def get_test_set(upscale_factor):
     crop_size = calculate_valid_crop_size(256, upscale_factor)
 
     return DatasetFromFolder(test_dir,
-                             input_transform=input_transform(crop_size, upscale_factor),
+                             input_transform=input_transform(
+                                 crop_size, upscale_factor),
                              target_transform=target_transform(crop_size))
