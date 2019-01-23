5a158e64f791077e2ca8823e1c0fb834a87410c2
ho7719 <ho7719@gmail.com>
2019-01-23 17:05:54 +0900

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -13,10 +13,12 @@ def load_img(filepath):
     img = Image.open(filepath).convert('YCbCr')
     return img
 
+
 class DatasetFromFolder(data.Dataset):
     def __init__(self, image_dir, input_transform=None, target_transform=None):
         super(DatasetFromFolder, self).__init__()
-        self.image_filenames = [join(image_dir, x) for x in listdir(image_dir) if is_image_file(x)]
+        self.image_filenames = [join(image_dir, x)
+                                for x in listdir(image_dir) if is_image_file(x)]
 
         self.input_transform = input_transform
         self.target_transform = target_transform
