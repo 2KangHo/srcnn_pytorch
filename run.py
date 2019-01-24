fa488636bec1cf2a6effd04ec5e1aa486c3564d3
ho7719 <ho7719@gmail.com>
2019-01-24 17:57:09 +0900

    Update run.py

--- run.py
+++ run.py
@@ -10,11 +10,15 @@ from torchvision.transforms import ToTensor
 import numpy as np
 
 # Training settings
-parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
-parser.add_argument('--input_image', type=str, required=True, help='input image to use')
-parser.add_argument('--model', type=str, required=True, help='model file to use')
-parser.add_argument('--output_filename', type=str, help='where to save the output image')
-parser.add_argument('--scale_factor', type=float, help='factor by which super resolution needed')
+parser = argparse.ArgumentParser(description='PyTorch SRCNN')
+parser.add_argument('--input_image', type=str,
+                    required=True, help='input image to use')
+parser.add_argument('--model', type=str, required=True,
+                    help='model file to use')
+parser.add_argument('--output_filename', type=str,
+                    help='where to save the output image')
+parser.add_argument('--scale_factor', type=float,
+                    help='factor by which super resolution needed')
 parser.add_argument('--cuda', action='store_true', help='use cuda')
 opt = parser.parse_args()
 
@@ -23,7 +27,8 @@ print(opt)
 img = Image.open(opt.input_image).convert('YCbCr')
 y, cb, cr = img.split()
 
-img = img.resize((int(img.size[0]*opt.scale_factor),int(img.size[1]*opt.scale_factor)),Image.BICUBIC)
+img = img.resize((int(img.size[0]*opt.scale_factor),
+                  int(img.size[1]*opt.scale_factor)), Image.BICUBIC)
 
 model_name = join("model", opt.model)
 model = torch.load(model_name)
@@ -36,7 +41,7 @@ if opt.cuda:
 out = model(input)
 out = out.cpu()
 
-print ("type = ",type(out))
+print("type = ", type(out))
 out_img_y = out.data[0].numpy()
 out_img_y *= 255.0
 out_img_y = out_img_y.clip(0, 255)
@@ -44,7 +49,8 @@ out_img_y = Image.fromarray(np.uint8(out_img_y[0]), mode='L')
 
 out_img_cb = cb.resize(out_img_y.size, Image.BICUBIC)
 out_img_cr = cr.resize(out_img_y.size, Image.BICUBIC)
-out_img = Image.merge('YCbCr', [out_img_y, out_img_cb, out_img_cr]).convert('RGB')
+out_img = Image.merge(
+    'YCbCr', [out_img_y, out_img_cb, out_img_cr]).convert('RGB')
 
 out_img.save(opt.output_filename)
 print('output image saved to ', opt.output_filename)
